9a1861bbcbe579e81e5729993ffec5b022b5f136
laft2 <44256206+laft2@users.noreply.github.com>
2024-08-12 12:44:25 +0000

    p63 day1まで

--- driver/loopback.c
+++ driver/loopback.c
@@ -28,9 +28,54 @@ struct loopback_queue_query {
 
 static int loopback_transmit(struct net_device *dev, uint16_t type,
                              const uint8_t *data, size_t len, const void *dst) {
+  struct loopback_queue_query *entry;
+  unsigned int num;
+
+  mutex_lock(&PRIV(dev)->mutex);
+  if (PRIV(dev)->queue.num >= LOOPBACK_QUEUE_LIMIT) {
+    mutex_unlock(&PRIV(dev)->mutex);
+    errorf("queue is full");
+    return -1;
+  }
+  entry = memory_alloc(sizeof(*entry) + len);
+  if (!entry) {
+    mutex_unlock(&PRIV(dev)->mutex);
+    errorf("memory_alloc() failure");
+    return -1;
+  }
+  entry->type = type;
+  entry->len = len;
+  memcpy(entry->data, data, len);
+  queue_push(&PRIV(dev)->queue, entry);
+  num = PRIV(dev)->queue.num;
+  mutex_unlock(&PRIV(dev)->mutex);
+  debugf("queue pushed (num:%u), dev=%s, type=0x%04x, len=%zd", num, dev->name,
+         type, len);
+  debugdump(data, len);
+  intr_raise_irq(PRIV(dev)->irq);
+  return 0;
 }
 
-static int loopback_isr(unsigned int irq, void *id) {}
+static int loopback_isr(unsigned int irq, void *id) {
+  struct net_device *dev;
+  struct loopback_queue_query *entry;
+
+  dev = (struct net_device *)id;
+  mutex_lock(&PRIV(dev)->mutex);
+  while (1) {
+    entry = queue_pop(&PRIV(dev)->queue);
+    if (!entry) {
+      break;
+    }
+    debugf("queue popped (num:%u), dev=%s, type=0x%04x, len=%zd",
+           PRIV(dev)->queue.num, dev->name, entry->type, entry->len);
+    debugdump(entry->data, entry->len);
+    net_input_handler(entry->type, entry->data, entry->len, dev);
+    memory_free(entry);
+  }
+  mutex_unlock(&PRIV(dev)->mutex);
+  return 0;
+}
 
 static struct net_device_ops loopback_ops = {
     .transmit = loopback_transmit,
