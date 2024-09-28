82f8af2e9cf027c925c31d6b4fe99a2a70876845
laft2 <44256206+laft2@users.noreply.github.com>
2024-09-28 15:54:03 +0000

    step13 完了

--- net.c
+++ net.c
@@ -5,6 +5,7 @@
 
 #include "platform.h"
 
+#include "arp.h"
 #include "icmp.h"
 #include "ip.h"
 #include "net.h"
@@ -238,6 +239,10 @@ void net_shutdown(void) {
 int net_init(void) {
   if (intr_init() == -1) {
     errorf("intr_init() failure");
+    return -1;
+  }
+  if (arp_init() == -1) {
+    errorf("arp_init() failure");
     return -1;
   }
   if (ip_init() == -1) {
