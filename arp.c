277574a07fedc24bef732b85cbcdb7aad2dad3e7
laft2 <44256206+laft2@users.noreply.github.com>
2024-09-29 14:24:35 +0000

    step16 雛形

--- arp.c
+++ arp.c
@@ -20,6 +20,7 @@
 #define ARP_OP_REPLY 2
 
 #define ARP_CACHE_SIZE 32
+#define ARP_CACHE_TIMEOUT 30 /* seconds */
 
 #define ARP_CACHE_STATE_FREE 0
 #define ARP_CACHE_STATE_INCOMPLETE 1
@@ -302,6 +303,8 @@ int arp_resolve(struct net_iface *iface, ip_addr_t pa, uint8_t *ha) {
          ether_addr_ntop(ha, addr2, sizeof(addr2)));
   return ARP_RESOLVE_FOUND;
 }
+
+static void arp_timer_handler(void) {}
 
 int arp_init(void) {
   if (net_protocol_register(NET_PROTOCOL_TYPE_ARP, arp_input) == -1) {
--- net.h
+++ net.h
@@ -80,6 +80,10 @@ extern int net_protocol_register(uint16_t type,
                                  void (*handler)(const uint8_t *data,
                                                  size_t len,
                                                  struct net_device *dev));
+
+extern int net_timer_register(struct timeval interval, void (*handler)(void));
+extern int net_timer_handler(void);
+
 extern int net_input_handler(uint16_t type, const uint8_t *data, size_t len,
                              struct net_device *dev);
 extern int net_softirq_handler(void);
