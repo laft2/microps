a35e4465d6719b1f1b0a15dd88ec334a39c0bbaf
laft2 <44256206+laft2@users.noreply.github.com>
2024-10-02 16:26:12 +0000

    step22 完了

--- net.c
+++ net.c
@@ -10,6 +10,7 @@
 #include "icmp.h"
 #include "ip.h"
 #include "net.h"
+#include "tcp.h"
 #include "udp.h"
 #include "util.h"
 
@@ -333,6 +334,10 @@ int net_init(void) {
   }
   if (udp_init() == -1) {
     errorf("udp_init() failure");
+    return -1;
+  }
+  if (tcp_init() == -1) {
+    errorf("tcp_init() failure");
     return -1;
   }
 
